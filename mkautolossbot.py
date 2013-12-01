7b89016d9be56598992ee2fa3fb728fc72dfb6ef
Gabriel Pacheco <gabriel@totendev.com>
2013-12-01 17:23:56 -0200

    insert first order to buy and sell shortcuts

--- mkautolossbot.py
+++ mkautolossbot.py
@@ -24,8 +24,11 @@ INITIAL_STOP_PRICE = 1
 STOP_PRICE_DELTA = 0
 # For Internal Bot usage - don't mess with these
 TRADE_TYPE = None
-MARKET_BUY = "Market Buy"
-MARKET_SELL = "Market Sell"
+TRADE_TYPE_MARKET_BUY = "Market Buy"
+TRADE_TYPE_MARKET_SELL = "Market Sell"
+TRADE_TYPE_FIRST_SELL_ORDER = "First Sell Order"
+TRADE_TYPE_FIRST_BUY_ORDER = "First Buy Order"
+SELL = "Market Sell"
 BTC = "BTC"
 LAST_TRADE_INFO = ""
 LAST_TRADE_PRICE_ASK = 0
@@ -33,6 +36,7 @@ LAST_TRADE_PRICE_SELL = 0
 TRIGGERED_TRADE_PRICE_SELL = 0
 STOP_PRICE = INITIAL_STOP_PRICE
 MINIMUM_BTC_WALLET_PRICE = 0.0001
+INSERT_ORDER_DIFFERENCE = 0.00001
 MKAUTOLOSSBOT_VERSION = "0.0.2"
 
 
@@ -60,6 +64,7 @@ class Strategy(strategy.Strategy):
         global LAST_TRADE_PRICE_BUY
         global STOP_PRICE_DELTA
         global STOP_PRICE
+        global TRADE_TYPE_MARKET_SELL
         
         if self.already_executed:
             Strategy.end_bot(self)
@@ -96,7 +101,7 @@ class Strategy(strategy.Strategy):
           # check stop loss decrease&sell
           if (LAST_TRADE_PRICE_SELL < STOP_PRICE) and (self.btc_wallet > MINIMUM_BTC_WALLET_PRICE):
             self.log("STOP LOSS activated !")
-            TRADE_TYPE = MARKET_SELL 
+            TRADE_TYPE = TRADE_TYPE_MARKET_SELL 
             #self.execute_trade()
             self.log_trade()
             #stop stop loss bot
@@ -105,37 +110,6 @@ class Strategy(strategy.Strategy):
             self.log_trade()
             #update status bar
             self.writeStatusInStatusBar(False)
-
-    def slot_keypress(self, gox, key):
-        global TRADE_TYPE
-        global STOP_PRICE
-        global STOP_PRICE_DELTA
-        if not self.init:
-            self.debug("Bot not initialized!")
-            return
-        key = chr(key)
-        self.log("GOT KEY %s" % (key))
-        if key == "k":
-             TRADE_TYPE = MARKET_BUY
-             self.execute_trade()
-             self.log_trade()
-        elif key == "m":
-             TRADE_TYPE = MARKET_SELL
-             self.execute_trade()
-             self.log_trade()
-             #stop stop loss bot
-             self.already_executed = True
-        elif key == "s":
-           dialog = StopLossDialog(STDSCR, self.gox, curses.color_pair(20), "AUTO STOP LOSS")
-           dialog.modal()
-           prc, delta = dialog.do_submit(dialog.price, dialog.delta)
-           if prc:
-                 STOP_PRICE = prc
-           if delta:
-                 STOP_PRICE_DELTA = delta                
-           #update info
-           self.log("STOP LOSS Changed by user to %.8f %s with delta %.8f" % (STOP_PRICE,self.user_currency,STOP_PRICE_DELTA))
-           self.writeStatusInStatusBar(False)
 
     def end_bot(self):
           global TRIGGERED_TRADE_PRICE_SELL
@@ -148,34 +122,7 @@ class Strategy(strategy.Strategy):
           self.log("STOP LOSS BOT DEACTIVATED!!!!!!")
           self.log_trade()
           return
-    ### trade methods
-    def execute_trade(self):
-      global LAST_TRADE_PRICE_SELL
-      global LAST_TRADE_PRICE_BUY
-      global TRADE_TYPE
-      global TRIGGERED_TRADE_PRICE_SELL
-      global LAST_TRADE_INFO
-      #reload prices
-      self.fetchPrices()
-      ###
-      if not self.init:
-          self.debug("Bot not initialized!")
-          return
           
-      if TRADE_TYPE == MARKET_BUY:
-        #
-        LAST_TRADE_INFO = " MARKET BUY: VOL %.8f %s -> %.8f BTC -- at %.8f %s" % (self.fiat_wallet, self.user_currency, (self.fiat_wallet / LAST_TRADE_PRICE_BUY),LAST_TRADE_PRICE_BUY,self.user_currency)
-        #
-        self.gox.buy(0, self.gox.base2int((self.fiat_wallet / LAST_TRADE_PRICE_BUY) * 1e8))
-        TRADE_TYPE = None
-      elif TRADE_TYPE == MARKET_SELL:
-        #
-        TRIGGERED_TRADE_PRICE_SELL = LAST_TRADE_PRICE_SELL
-        LAST_TRADE_INFO = " MARKET SELL: VOL %.8f BTC -> %.8f %s -- at %.8f %s" % (self.btc_wallet, (self.btc_wallet * LAST_TRADE_PRICE_SELL),self.user_currency,LAST_TRADE_PRICE_SELL,self.user_currency)
-        #
-        self.gox.sell(0, self.gox.base2int(self.btc_wallet * 1e8))
-        TRADE_TYPE = None
-        
     def fecthWallet(self):
       global BTC
       self.btc_wallet = goxapi.int2float(self.gox.wallet[BTC], BTC)
@@ -190,19 +137,18 @@ class Strategy(strategy.Strategy):
       LAST_TRADE_PRICE_BUY = float(self.gox.orderbook.ask/float(100000))
       # log
       self.log("Got last price SELL: %.8f %s BUY: %.8f %s" % (LAST_TRADE_PRICE_SELL,self.user_currency,LAST_TRADE_PRICE_BUY,self.user_currency))
+      
     #display info methods
     def log_trade(self):
       global LAST_TRADE_INFO
-      if LAST_TRADE_INFO != "":
-        self.log(LAST_TRADE_INFO)
-        
+      self.log(LAST_TRADE_INFO)
     def log(self, msg):
-      self.debug("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-      self.debug("                                                                                 ")
-      self.debug(15 * " " + msg)
-      self.debug("                                                                                 ")
-      self.debug("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-      
+      if msg != "":
+        self.debug("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+        self.debug("                                                                                 ")
+        self.debug(15 * " " + msg)
+        self.debug("                                                                                 ")
+        self.debug("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
     def writeStatusInStatusBar(self,isTriggered):
       global MINIMUM_BTC_WALLET_PRICE
       global STOP_PRICE
@@ -212,7 +158,100 @@ class Strategy(strategy.Strategy):
                   else ("ACTIVE" if (self.btc_wallet > MINIMUM_BTC_WALLET_PRICE) \
                   else "INACTIVE"))
       self.statuswin.addStrategyInformation(" | Stop Loss (%s): VALUE %.4f DELTA %.4f" % (BOT_STATE,STOP_PRICE,STOP_PRICE_DELTA))
-        
+      
+      ### actions
+      def execute_trade(self):
+        global LAST_TRADE_PRICE_SELL
+        global LAST_TRADE_PRICE_BUY
+        global TRIGGERED_TRADE_PRICE_SELL
+        global TRADE_TYPE
+        global TRADE_TYPE_MARKET_BUY
+        global TRADE_TYPE_MARKET_SELL
+        global TRADE_TYPE_FIRST_SELL_ORDER
+        global TRADE_TYPE_FIRST_BUY_ORDER
+        global INSERT_ORDER_DIFFERENCE
+        global LAST_TRADE_INFO
+        #reload prices
+        self.fetchPrices()
+        ###
+        if not self.init:
+            self.debug("Bot not initialized!")
+            return
+        if TRADE_TYPE == TRADE_TYPE_MARKET_BUY:
+          #Write Info
+          LAST_TRADE_INFO = " MARKET BUY: VOL %.8f %s -> %.8f BTC -- at %.8f %s" % \
+          (self.fiat_wallet, self.user_currency, (self.fiat_wallet / LAST_TRADE_PRICE_BUY),LAST_TRADE_PRICE_BUY,self.user_currency)
+          #Order
+          self.gox.buy(0, self.gox.base2int((self.fiat_wallet / LAST_TRADE_PRICE_BUY) * 1e8))
+          TRADE_TYPE = None
+        elif TRADE_TYPE == TRADE_TYPE_MARKET_SELL:
+          #Write Info
+          TRIGGERED_TRADE_PRICE_SELL = LAST_TRADE_PRICE_SELL
+          LAST_TRADE_INFO = " MARKET SELL: VOL %.8f BTC -> %.8f %s -- at %.8f %s" % \
+          (self.btc_wallet, (self.btc_wallet * LAST_TRADE_PRICE_SELL),self.user_currency,LAST_TRADE_PRICE_SELL,self.user_currency)
+          #Order
+          self.gox.sell(0, self.gox.base2int(self.btc_wallet * 1e8))
+          TRADE_TYPE = None
+        elif TRADE_TYPE == TRADE_TYPE_FIRST_SELL_ORDER:
+          #Write Info
+          tradeValue = LAST_TRADE_PRICE_SELL-INSERT_ORDER_DIFFERENCE
+          LAST_TRADE_INFO = " SELL ORDER: VOL %.8f BTC -> %.8f %s -- at %.8f %s" % \
+          (self.btc_wallet, (self.btc_wallet * tradeValue),self.user_currency,tradeValue,self.user_currency)
+          #Order
+          self.gox.sell(tradeValue, self.gox.base2int(self.btc_wallet * 1e8))
+          TRADE_TYPE = None
+        elif TRADE_TYPE == TRADE_TYPE_FIRST_BUY_ORDER:
+          #Write Info
+          tradeValue = LAST_TRADE_PRICE_BUY+INSERT_ORDER_DIFFERENCE
+          LAST_TRADE_INFO = " MARKET BUY: VOL %.8f %s -> %.8f BTC -- at %.8f %s" % \
+          (self.fiat_wallet, self.user_currency, (self.fiat_wallet / tradeValue),tradeValue,self.user_currency)
+          #Order
+          self.gox.buy(tradeValue, self.gox.base2int((self.fiat_wallet / tradeValue) * 1e8))
+          TRADE_TYPE = None
+          
+      def slot_keypress(self, gox, key):
+          global TRADE_TYPE
+          global STOP_PRICE
+          global STOP_PRICE_DELTA
+          global TRADE_TYPE_MARKET_BUY
+          global TRADE_TYPE_MARKET_SELL
+          global TRADE_TYPE_FIRST_SELL_ORDER
+          global TRADE_TYPE_FIRST_BUY_ORDER
+          if not self.init:
+              self.debug("Bot not initialized!")
+              return
+          key = chr(key)
+          self.log("GOT KEY %s" % (key))
+          if key == "k":
+               TRADE_TYPE = TRADE_TYPE_MARKET_BUY
+               self.execute_trade()
+               self.log_trade()
+          elif key == "m":
+               TRADE_TYPE = TRADE_TYPE_MARKET_SELL
+               self.execute_trade()
+               self.log_trade()
+               #stop stop loss bot
+               self.already_executed = True
+          elif key == "a":
+             TRADE_TYPE = TRADE_TYPE_FIRST_SELL_ORDER
+             self.execute_trade()
+             self.log_trade()
+          elif key == "c":
+             TRADE_TYPE = TRADE_TYPE_FIRST_BUY_ORDER
+             self.execute_trade()
+             self.log_trade()
+          elif key == "s":
+             dialog = StopLossDialog(STDSCR, self.gox, curses.color_pair(20), "AUTO STOP LOSS")
+             dialog.modal()
+             prc, delta = dialog.do_submit(dialog.price, dialog.delta)
+             if prc:
+                   STOP_PRICE = prc
+             if delta:
+                   STOP_PRICE_DELTA = delta                
+             #update info
+             self.log("STOP LOSS Changed by user to %.8f %s with delta %.8f" % (STOP_PRICE,self.user_currency,STOP_PRICE_DELTA))
+             self.writeStatusInStatusBar(False)
+            
 class StopLossDialog(goxtool.DlgStopLoss):
     def __init__(self, stdscr, gox, color, msg):
         goxtool.DlgStopLoss.__init__(self, stdscr, gox, color, msg)
